bf09b0a6500f4da4c9901c8dd74523126c4813f3
TaylanCann <taylancanh@gmail.com>
2023-06-20 23:05:12 +0300

    fix for db

--- ResxToPostgreSQL/Program.cs
+++ ResxToPostgreSQL/Program.cs
@@ -17,6 +17,38 @@ public class Program
             connection.Open();
 
             // Verileri ekleme
+            string sql = "INSERT INTO \"NEMS_COMMON\".\"COMMON_RESOURCES\" (\"KEY\", \"VALUE\" , \"LANGUAGE_KEY\", \"TENANT_ID\" ) VALUES (@key, @value , 'tr' , 1)";
+
+            using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+            {
+                // Language.resx dosyasını okuma
+                var doc = new XmlDocument();
+                doc.Load(@"D:\Dev\Git\onent\onent_core\spa\Resources\Language.tr-TR.resx");
+                XmlNodeList dataNodes = doc.SelectNodes("//data");
+
+                foreach (XmlNode dataNode in dataNodes)
+                {
+                    string key = dataNode.Attributes["name"].Value;
+                    string value = dataNode.SelectSingleNode("value").InnerText;
+
+                    command.Parameters.Clear();
+                    command.Parameters.AddWithValue("@KEY", key);
+                    command.Parameters.AddWithValue("@VALUE", value);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // Veritabanı bağlantısını kapatma
+            connection.Close();
+        }
+
+        // Veritabanı bağlantısı
+        using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+
+            // Verileri ekleme
             string sql = "INSERT INTO \"NEMS_COMMON\".\"COMMON_RESOURCES\" (\"KEY\", \"VALUE\" , \"LANGUAGE_KEY\", \"TENANT_ID\" ) VALUES (@key, @value , 'default' , 1)";
 
             using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
@@ -44,36 +76,6 @@ public class Program
         }
 
 
-        // Veritabanı bağlantısı
-        using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
-        {
-            connection.Open();
-
-            // Verileri ekleme
-            string sql = "INSERT INTO \"NEMS_COMMON\".\"COMMON_RESOURCES\" (\"KEY\", \"VALUE\" , \"LANGUAGE_KEY\", \"TENANT_ID\" ) VALUES (@key, @value , 'tr' , 1)";
-
-            using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
-            {
-                // Language.resx dosyasını okuma
-                var doc = new XmlDocument();
-                doc.Load(@"D:\Dev\Git\onent\onent_core\spa\Resources\Language.tr-TR.resx");
-                XmlNodeList dataNodes = doc.SelectNodes("//data");
-
-                foreach (XmlNode dataNode in dataNodes)
-                {
-                    string key = dataNode.Attributes["name"].Value;
-                    string value = dataNode.SelectSingleNode("value").InnerText;
-
-                    command.Parameters.Clear();
-                    command.Parameters.AddWithValue("@KEY", key);
-                    command.Parameters.AddWithValue("@VALUE", value);
-
-                    command.ExecuteNonQuery();
-                }
-            }
-
-            // Veritabanı bağlantısını kapatma
-            connection.Close();
-        }
+      
     }
 }
